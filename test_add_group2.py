00c793365e8c585646a742d65a602ac8f512814a
Назарова Софья <nsonya28@gmail.com>
2023-11-20 23:09:26 +0300

    Почти задание 2

--- test_add_group2.py
+++ test_add_group2.py
@@ -1,42 +1,75 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 
-class TestAddGroup2(unittest.TestCase):
+class test_add_group2(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
 
+    def open_home_paqe(self, wd):
+        wd.get("http://localhost/addressbook/")
+
+
+    def loqin(self, wd, username, password):
+        wd.find_element("name", "user").clear()
+        wd.find_element("name", "user").send_keys(username)
+        wd.find_element("name", "pass").click()
+        wd.find_element("name", "pass").clear()
+        wd.find_element("name", "pass").send_keys(password)
+        wd.find_element("xpath", "//input[@value='Login']").click()
+
+
+    def open_groups_page(self, wd):
+        wd.find_element("link text", "groups").click()
+
+
+    def create_group(self, wd, name, header, footer):
+        # init group creation
+        wd.find_element("name", "new").click()
+        # fill group form
+        wd.find_element("name", "group_name").click()
+        wd.find_element("name", "group_name").clear()
+        wd.find_element("name", "group_name").send_keys(name)
+        wd.find_element("name", "group_header").click()
+        wd.find_element("name", "group_header").clear()
+        wd.find_element("name", "group_header").send_keys(header)
+        wd.find_element("name", "group_footer").click()
+        wd.find_element("name", "group_footer").clear()
+        wd.find_element("name", "group_footer").send_keys(footer)
+        # submit group creation
+        wd.find_element("name", "submit").click()
+
+
+    def return_to_group_page(self, wd):
+        wd.find_element("link text", "group page").click()
+
+
+    def logout(self, wd):
+        wd.find_element("link text", "Logout").click()
+
     def test_add_group2(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
-        wd.find_element("name","user").clear()
-        wd.find_element("name","user").send_keys("admin")
-        wd.find_element("name","pass").click()
-        wd.find_element("name","pass").clear()
-        wd.find_element("name","pass").send_keys("secret")
-        wd.find_element("xpath","//input[@value='Login']").click()
-        wd.find_element("name","new").click()
-        wd.find_element("name","group_name").click()
-        wd.find_element("name","group_name").clear()
-        wd.find_element("name","group_name").send_keys("2")
-        wd.find_element("name","group_header").click()
-        wd.find_element("name","group_header").clear()
-        wd.find_element("name","group_header").send_keys("234")
-        wd.find_element("name","group_footer").click()
-        wd.find_element("name","group_footer").clear()
-        wd.find_element("name","group_footer").send_keys("2345")
-        wd.find_element("name","submit").click()
-        wd.find_element("link text","group page").click()
-        wd.find_element("link text","Logout").click()
+        self.open_home_paqe(wd)
+        self.loqin(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, name="2", header="234", footer="2345")
+        self.return_to_group_page(wd)
+        self.logout(wd)
 
+
+    def test_add_empty_group2(self):
+        wd = self.wd
+        self.open_home_paqe(wd)
+        self.loqin(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, name="", header="", footer="")
+        self.return_to_group_page(wd)
+        self.logout(wd)
 
     def is_element_present(self, how, what):
         try:
