1a0636991537b1c0b361bcc354ec3c010b36f74d
Назарова Софья <nsonya28@gmail.com>
2023-11-20 23:45:55 +0300

    Почти задание 3

--- test_add_group2.py
+++ test_add_group2.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from group import Group
 
 
 class test_add_group2(unittest.TestCase):
@@ -28,19 +29,19 @@ class test_add_group2(unittest.TestCase):
         wd.find_element("link text", "groups").click()
 
 
-    def create_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         # init group creation
         wd.find_element("name", "new").click()
         # fill group form
         wd.find_element("name", "group_name").click()
         wd.find_element("name", "group_name").clear()
-        wd.find_element("name", "group_name").send_keys(name)
+        wd.find_element("name", "group_name").send_keys(group.name)
         wd.find_element("name", "group_header").click()
         wd.find_element("name", "group_header").clear()
-        wd.find_element("name", "group_header").send_keys(header)
+        wd.find_element("name", "group_header").send_keys(group.header)
         wd.find_element("name", "group_footer").click()
         wd.find_element("name", "group_footer").clear()
-        wd.find_element("name", "group_footer").send_keys(footer)
+        wd.find_element("name", "group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element("name", "submit").click()
 
@@ -57,7 +58,7 @@ class test_add_group2(unittest.TestCase):
         self.open_home_paqe(wd)
         self.loqin(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, name="2", header="234", footer="2345")
+        self.create_group(wd, Group(name="2", header="234", footer="2345"))
         self.return_to_group_page(wd)
         self.logout(wd)
 
@@ -67,7 +68,7 @@ class test_add_group2(unittest.TestCase):
         self.open_home_paqe(wd)
         self.loqin(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, name="", header="", footer="")
+        self.create_group(wd, Group(name="", header="", footer=""))
         self.return_to_group_page(wd)
         self.logout(wd)
 
