8ba2ddc245d1bf637da556571f3acec8012b5fb9
Назарова Софья <nsonya28@gmail.com>
2023-12-17 20:32:55 +0300

    Задание 13 (исправленное)

--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -8,9 +8,9 @@ def test_modify_first_contact_firstname(app):
     old_contacts = app.contact.get_contact_list()
     index = randrange(len(old_contacts))
     contact = Contact(nickname="old nickname")
-    contact.id = old_contacts[0].id
-    app.contact.modify_contact_by_index(index,contact)
+    contact.id = old_contacts[index].id
+    app.contact.modify_contact_by_index(index, contact)
     assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    old_contacts[0] = contact
+    old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
