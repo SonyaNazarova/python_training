2aa92210549b746f0ae31c4ca448792402f3dce1
Назарова Софья <nsonya28@gmail.com>
2023-12-24 19:43:04 +0300

    Задание 13 (исправление 6)

--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -4,10 +4,10 @@ from random import randrange
 
 def test_modify_first_contact_firstname(app):
     if app.contact.count() == 0:
-        app.contact.create(Contact(nickname="test"))
+        app.contact.create(Contact(lastname="test"))
     old_contacts = app.contact.get_contact_list()
     index = randrange(len(old_contacts))
-    contact = Contact(nickname="old nickname")
+    contact = Contact(lastname="old lastname")
     contact.id = old_contacts[index].id
     app.contact.modify_contact_by_index(index, contact)
     assert len(old_contacts) == app.contact.count()
