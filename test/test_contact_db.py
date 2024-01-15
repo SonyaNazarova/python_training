82e1b8c3520d7635f1a47720cfe9234121ced450
Назарова Софья <nsonya28@gmail.com>
2024-01-15 11:21:26 +0300

    Задание 21(исправление 3)

--- test/test_contact_db.py
+++ test/test_contact_db.py
@@ -5,13 +5,12 @@ from random import randrange
 def test_contact(app, db):
     old_contacts = db.get_contact_list()
     for contact in old_contacts:
-        assert contact.lastname == app.contact.get_contact_info_from_home_page_id(contact.id).lastname
-        assert contact.firstname == app.contact.get_contact_info_from_home_page_id(contact.id).firstname
-        assert contact.address == app.contact.get_contact_info_from_home_page_id(contact.id).address
-        assert merge_phones_like_on_home_page(contact) == app.contact.get_contact_info_from_home_page_id(
-            contact.id).all_phones_from_home_page
-        assert merge_emails_on_home_page(contact) == app.contact.get_contact_info_from_home_page_id(
-            contact.id).all_emails
+        contact_from_home_page = app.contact.get_contact_info_from_home_page_id(contact.id)
+        assert contact.lastname == contact_from_home_page.lastname
+        assert contact.firstname == contact_from_home_page.firstname
+        assert contact.address == contact_from_home_page.address
+        assert merge_phones_like_on_home_page(contact) == contact_from_home_page.all_phones_from_home_page
+        assert merge_emails_on_home_page(contact) == contact_from_home_page.all_emails
 
 
 def merge_phones_like_on_home_page(contact):
